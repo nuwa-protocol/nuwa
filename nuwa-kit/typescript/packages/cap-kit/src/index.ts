a3b4089ef3fd87d7cb8e5db70457f8fa6d07f180
sven <sven.tan@icloud.com>
2025-09-15 15:52:37 +0800

    0.6

--- nuwa-kit/typescript/packages/cap-kit/src/index.ts
+++ nuwa-kit/typescript/packages/cap-kit/src/index.ts
@@ -130,7 +130,7 @@ export class CapKit {
 			const transformedItems = queryResult.data.items.map((item: any) => {
 				return {
 					...item,
-					thumbnail: item,
+					// thumbnail: item,
 				};
 			});
 
@@ -185,7 +185,30 @@ export class CapKit {
 				);
 			}
 
-			return queryResult;
+			const transformedItems = queryResult.data.items.map((item: any) => {
+				return {
+					...item,
+					displayName: item.display_name,
+					stats: {
+						downloads: item.cap_stats.downloads,
+						capId: item.cap_stats.cap_id,
+						ratingCount: item.cap_stats.rating_count,
+						averageRating: item.cap_stats.average_rating,
+						userRating: item.cap_stats.user_rating,
+						favorites: item.cap_stats.favorites,
+					}
+				} as ResultCap;
+			});
+
+			return {
+				code: queryResult.code,
+				data: {
+					totalItems: queryResult.data.totalItems,
+					page: queryResult.data.page,
+					pageSize: queryResult.data.pageSize,
+					items: transformedItems,
+				},
+			} as Result<Page<ResultCap>>;
 		} finally {
 			await client.close();
 		}
