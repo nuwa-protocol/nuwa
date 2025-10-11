383ec67a96aaaabc5df7a8510c723cff1797b25a
jolestar <jolestar@gmail.com>
2025-10-11 19:11:44 +0800

    remove archives (#404)
    * remove archives
    
    * update readme

--- archives/web/app/src/utils/icon.ts
+++
@@ -1,2 +0,0 @@
-export const normalizeCoinIconUrl = (onChainCoinIconUrl?: string | null) =>
-  `data:image/svg+xml;utf8,${encodeURIComponent(onChainCoinIconUrl || '')}`;
--- archives/web/campaign/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-// https://next-auth.js.org/configuration/nextjs#app-directory
-
-import NextAuth from "next-auth"
-import { authOptions } from "@/auth"
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
