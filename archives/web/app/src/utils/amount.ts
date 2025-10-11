a879a080e4704397d5f26d0af3b0fb18ef52c53f
jolestar <jolestar@gmail.com>
2025-10-11 19:04:00 +0800

    remove archives

--- archives/web/app/src/utils/amount.ts
+++
@@ -1,8 +0,0 @@
-import { BigNumber } from "bignumber.js";
-
-export const formatAmountDisplay = (
-  amount: number | string | bigint,
-  decimals?: number
-) => {
-  return new BigNumber(amount.toString()).toFormat(decimals ?? 2);
-};
--- archives/web/campaign/src/app/components/chat/message/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './styles';
-export * from './markdownComponents';
-export * from './ToolInvocationContent';
-export * from './MessageContent';
-export * from './Avatar';
